5368c8d4e2d77c2daf8f01c82384336534136887
Serj Myakotnikov <geniu212@bk.ru>
2021-01-24 21:56:00 +0400

    Product Create

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function product()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
