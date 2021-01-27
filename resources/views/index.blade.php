20ed8dbc7ac7dff90b76c5a5163e025c9372efe8
Serj Myakotnikov <geniu212@bk.ru>
2021-01-27 21:44:37 +0400

    Minor Cosmetic Update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,21 +7,6 @@
     </x-slot>
 
 
-@if (Route::has('login'))
-        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            @auth
-                <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Покупатель</a>
-                <a href="{{ url('/dashboard/seller') }}" class="text-sm text-gray-700 underline">Продавец</a>
-            @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Войти</a>
-
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Регистрация</a>
-                @endif
-            @endauth
-        </div>
-    @endif
-
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 
         <table class="table table-striped" dusk="product-table">
--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum', 'verified'])->get ('/',[ProductController::class, 'index'] )->name('index');
+Route::middleware(['auth:sanctum', 'verified'])->get('/', [ProductController::class, 'index'])->name('index');
 
-Route::post('/product/create',[ProductController::class, 'store'] )->name('store');
+Route::post('/product/create', [ProductController::class, 'store'])->name('store');
 
 //позде этот роут будет вести на заказы покупателя
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', [ProductController::class, 'for_user'])->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard/seller', [ProductController::class, 'for_user'])->name('seller');
-Route::get ('product/{id}/edit',[ProductController::class, 'edit'] )->name('edit_product');
-Route::get ('product/{id}',[ProductController::class, 'show'] )->name('show_product');
-Route::patch ('product/{id}/update',[ProductController::class, 'update'] )->name('update_product');
-Route::delete ('product/{id}/destroy',[ProductController::class, 'destroy'] )->name('destroy_product');
+Route::get('product/{id}/edit', [ProductController::class, 'edit'])->name('edit_product');
+Route::get('product/{id}', [ProductController::class, 'show'])->name('show_product');
+Route::patch('product/{id}/update', [ProductController::class, 'update'])->name('update_product');
+Route::delete('product/{id}/destroy', [ProductController::class, 'destroy'])->name('destroy_product');
