2411f90199efb87fc97916e8dfa3d4e3ad810278
Serj Myakotnikov <geniu212@bk.ru>
2021-02-03 22:58:16 +0400

    Cart Redesign

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -6,85 +6,110 @@
     </x-slot>
     <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
         @include('layouts.errors')
-        @php
-           // echo '<pre>';
-         //  var_dump(session('cart'));
 
-        @endphp
-        <table class="table table-striped" dusk="product-table">
-            <thead>
+
             @php
-             $products = session('cart')->getProducts();
+                $products = session('cart')->getProducts();
             @endphp
-            @foreach($products as $product)
 
-            </thead>
-            <tbody>
-            <tr>
-                <td>{{$product->getName()}}</td>
-                <td><img src="{{$product->getPhoto()}}" width="120px" height="120px"></td>
-                <td>{{$product->getPrice()}} &#8381;</td>
-                <td>{{$product->getFullSpec()}} </td>
+    </div>
+    <div class="bg-gray-100">
+        <div class="container mx-auto mt-10">
+            <div class="flex shadow-md my-10">
+                <div class="w-3/4 bg-white px-10 py-10">
+                    <div class="flex justify-between border-b pb-8">
+                        <h1 class="font-semibold text-2xl">Товаров в корзине:</h1>
+                        <h2 class="font-semibold text-2xl">{{count($products)}}</h2>
+                    </div>
+                    <div class="flex justify-evenly mt-10 mb-5">
+                        <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Товар</h3>
+{{--                        <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">--}}
+{{--                            Количество</h3>--}}
+                        <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">
+                            Цена</h3>
+{{--                        <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">--}}
+{{--                            Сумма</h3>--}}
+                    </div>
+                    @if($products == null)
+                    <h1 class="text-center font-semibold text-2xl">Корзина пуста</h1>
+                    @else
+                    @foreach($products as $product)
+                    <div class="flex justify-evenly hover:bg-gray-100 -mx-8 px-6 py-5">
+                        <div class="flex w-2/5"> <!-- product -->
+                            <div class="w-20">
+                                <img class="h-24" src="{{$product->getPhoto()}}"
+                                     alt="">
+                            </div>
+                            <div class="flex flex-col justify-between ml-4 flex-grow">
+                                <span class="font-bold text-sm">{{$product->getName()}}</span>
 
-            </tr>
+                                <a href="{{route('show_product',$product->getId())}}" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Подробнее</a>
 
-            <td>
-                <form action="{{ route('remove_from_cart', $product->getId())}}" method="post">
-                    @csrf
-                    <button class="btn btn-danger" type="submit" dusk="delete-button">Удалить</button>
-                </form>
-            </td>
-            @endforeach
-            </tbody>
-        </table>
+                                <form action="{{ route('remove_from_cart', $product->getId())}}" method="post">
+                                    @csrf
+                                <button class="font-semibold hover:text-red-500 text-gray-500 text-xs">Удалить</button>
+                                </form>
+                            </div>
+                        </div>
+{{--                        <div class="flex justify-center w-1/5">--}}
+{{--                            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">--}}
+{{--                                <path--}}
+{{--                                    d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>--}}
+{{--                            </svg>--}}
 
+{{--                            <input class="mx-2 border text-center w-8" type="text" value="1">--}}
 
-        @if($products == null)
-        Корзина пуста.
-        @else
-            <br>
-            Итоговая сумма: {{$totalPrice}} &#8381;
-            <br>
-        <form action="{{ route('actualize_cart')}}" method="POST" class="form-horizontal">
-            @csrf
+{{--                            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">--}}
+{{--                                <path--}}
+{{--                                    d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>--}}
+{{--                            </svg>--}}
+{{--                        </div>--}}
+                        <span class="text-center w-1/5 font-semibold text-sm">{{$product->getPrice()}} &#8381;</span>
+{{--                        <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>--}}
+                    </div>
+                    @endforeach
+                    @endif
 
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Актуализировать
-                    </button>
+                    <a href="{{route('index')}}" class="flex font-semibold text-indigo-600 text-sm mt-10">
+
+                        <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512">
+                            <path
+                                d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/>
+                        </svg>
+                        Продолжить покупки
+                    </a>
+                    <hr>
+                    <form action="{{ route('actualize_cart')}}" method="POST">
+                        @csrf
+                    <button class="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase w-full">Обновить цены</button>
+                    </form>
+                    <hr>
+                    <form action="{{ route('clear_cart')}}" method="POST">
+                        @csrf
+                    <button  class="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase w-full">Очистить корзину</button>
+                    </form>
                 </div>
-            </div>
-        </form>
 
-        <form action="{{ route('clear_cart')}}" method="POST" class="form-horizontal">
-            @csrf
+                <div id="summary" class="w-1/4 px-8 py-10">
+                    <h1 class="font-semibold text-2xl">Заказ</h1>
+                    <div class="border-t mt-8">
+                        <div class="flex font-semibold justify-between py-6 text-sm uppercase">
+                            <span>Итоговая сумма:</span>
+                            <span> {{$totalPrice}} &#8381;</span>
+                        </div>
+                        <form action="{{ route('cart_to_order')}}" method="POST">
+                            @csrf
+                        <button
+                            class="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
+                            Оформить
+                        </button>
+                        </form>
+                    </div>
 
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Очистить Корзину
-                    </button>
                 </div>
+
             </div>
-        </form>
-
-        <form action="{{ route('cart_to_order')}}" method="POST" class="form-horizontal">
-            @csrf
-
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Оформить Заказ
-                    </button>
-                </div>
-            </div>
-        </form>
-        @endif
-
-
-
-
+        </div>
     </div>
 
 </x-app-layout>
